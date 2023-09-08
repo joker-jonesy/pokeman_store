9087256701d6fd97f82793eafdcc7ddc6242c22f
joker-jonesy <spashmcgrath94@yahoo.com>
2023-09-08 11:48:10 -0400

    kermit

--- src/main.jsx
+++ src/main.jsx
@@ -4,27 +4,22 @@ import './index.css'
 import {Provider} from "react-redux";
 import store from "./store.js";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import {ApiProvider} from "@reduxjs/toolkit/dist/query/react";
 import PokemonPage from "./pages/pokemon/PokemonPage.jsx";
 import SinglePokemonPage from "./pages/spokemon/SinglePokemonPage.jsx";
 import CartPage from "./pages/cart/CartPage.jsx";
 import Nav from "./components/Nav.jsx";
-import {pokemonApi} from "./reducers/pokemon.js";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <ApiProvider api={pokemonApi}>
-                <BrowserRouter>
-                    <Nav/>
-                    <Routes>
-                        <Route index element={<PokemonPage/>}/>
-                        <Route path={"/cart"} element={<CartPage/>}/>
-                        <Route path={"/pokemon/:id"} element={<SinglePokemonPage/>}/>
-                    </Routes>
-                </BrowserRouter>
-            </ApiProvider>
-        </Provider>
-
-    </React.StrictMode>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+          <Nav/>
+        <Routes>
+          <Route index element={<PokemonPage/>}/>
+          <Route path={"/cart"} element={<CartPage/>}/>
+          <Route path={"/pokemon/:id"} element={<SinglePokemonPage/>}/>
+        </Routes>
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
--- src/store.js
+++ src/store.js
@@ -1,17 +1,16 @@
 import {configureStore} from "@reduxjs/toolkit";
-// import { setupListeners } from '@reduxjs/toolkit/query'
+import { setupListeners } from '@reduxjs/toolkit/query'
 import cartReducer from './reducers/cart.js';
-// import {pokemonApi} from "./reducers/pokemon.js";
+import {pokemonApi} from "./reducers/pokemon.js";
 
 const store = configureStore({
     reducer:{
         cart: cartReducer,
-        // [pokemonApi.reducerPath]: pokemonApi.reducer
-
+        pokemonApi: pokemonApi.reducer
     },
-    // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(pokemonApi.middleware)
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(pokemonApi.middleware)
 })
 
-// setupListeners(store.dispatch)
+setupListeners(store.dispatch)
 
 export default store;
