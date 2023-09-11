d8dacfe70154b28432275e9acbe67dacfd0be538
joker-jonesy <spashmcgrath94@yahoo.com>
2023-09-11 12:08:32 -0400

    kermit

--- src/pages/pokemon/PokemonPage.jsx
+++ src/pages/pokemon/PokemonPage.jsx
@@ -1,15 +1,20 @@
 import Content from "../../components/Content.jsx";
 import {useGetPokemonQuery} from "../../reducers/pokemon.js";
 import SinglePokemon from "../../components/SinglePokemon.jsx";
+import {useState} from "react";
 
 function PokemonPage(){
 
     const {data, isLoading} = useGetPokemonQuery();
 
+    const [input,setInput]=useState("");
+
     return (
         <>
             <Content>
-                {isLoading?<h1>Loading....</h1>:data.results.map((i,idx)=>
+                <input type={"text"} onChange={(event)=>setInput(event.target.value)}/>
+                {isLoading?<h1>Loading....</h1>: input.trim().length>0? data.results.filter(i=>i.name.includes(input)).map((i,idx)=>
+                    <SinglePokemon key={idx} cart={false} name={i.name}/>) :data.results.map((i,idx)=>
                     <SinglePokemon key={idx} cart={false} name={i.name}/>
                 )}
             </Content>
